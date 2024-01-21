9890865929765fcaf3f434d2f172b28418e7fa74
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-01-20 23:59:14 -0500

    Update ModelCallback print statement

--- training.py
+++ training.py
@@ -11,9 +11,10 @@ from sheets import log
 
 class ModelCallback(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
-        print("\nEpoch " + str(epoch) + " done! RAM Usage: " + \
-            str(round(psutil.Process().memory_info().rss/ 1024 ** 2)) + " mb (total: " + \
-            str(round(psutil.virtual_memory().percent, 1)) + "%)")
+        print("\nEpoch " + str(epoch) + " done! \n\tRAM Usage: " + \
+            str(round(psutil.Process().memory_info().rss/ 1024 ** 2)) + " mb \n\tTotal RAM Usage: " + \
+            str(round(psutil.virtual_memory().percent, 1)) + "%)\n\tCPU Usage: " + \
+            str(round(psutil.cpu_percent()/100, 1)) + "%")
         if(epoch % 10 == 0):
             log("Epoch " + str(epoch) + " done!")
 
